944feae5c59e00e02e2e1286e7a06caf498901a2
Vidura Perera <viduradilshan19970729@gmail.com>
2024-07-18 14:01:19 +0530

    first commit

---
+++ database.sql
@@ -0,0 +1,18 @@
+create database ajaxform; 
+
+use ajaxform;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(100)
+);
+
+DESC user;
+
+SELECT * FROM user;
+
+
+
+
